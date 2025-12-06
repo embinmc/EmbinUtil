f9bc0324b5087c460214c566bb84b89e63c89d7d
Embin <113154360+NotEmbin@users.noreply.github.com>
2025-12-05 18:58:42 -0800

    should've tested it... (not testing this either)

--- src/main/java/embinmc/lib/util/codec/SerializableEnum.java
+++ src/main/java/embinmc/lib/util/codec/SerializableEnum.java
@@ -4,19 +4,19 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.DataResult;
 
 @SuppressWarnings({"unused"})
-public interface SerializableEnum {
+public interface SerializableEnum<T extends Enum<T>> {
     String asString();
-    <T> Codec<T> codec();
+    Codec<T> codec();
 
     default boolean isString(String string) {
         return this.asString().equals(string);
     }
 
-    static <T extends Enum<T> & SerializableEnum> Codec<T> getCodec(Class<T> clazz) {
+    static <T extends Enum<T> & SerializableEnum<T>> Codec<T> getCodec(Class<T> clazz) {
         return Codec.STRING.comapFlatMap(s -> validate(s, clazz), T::asString);
     }
 
-    private static <T extends Enum<T> & SerializableEnum> DataResult<T> validate(String value, Class<T> clazz) {
+    private static <T extends Enum<T> & SerializableEnum<T>> DataResult<T> validate(String value, Class<T> clazz) {
         try {
             return DataResult.success(T.valueOf(clazz, value.toUpperCase()));
         } catch (IllegalArgumentException _) {
