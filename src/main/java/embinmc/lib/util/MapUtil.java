48c696150db8e8741b969d1e664f92da3af28049
Embin <113154360+NotEmbin@users.noreply.github.com>
2025-12-27 12:44:54 -0800

    Update MapUtil.java

--- src/main/java/embinmc/lib/util/MapUtil.java
+++ src/main/java/embinmc/lib/util/MapUtil.java
@@ -14,18 +14,34 @@ public final class MapUtil {
         return defaultValue;
     }
 
-    public static <Key, Value> boolean hasKeyIdMap(Map<Key, Value> map, Key key) {
+    public static <Key, Value> boolean hasKey(Map<Key, Value> map, Key key) {
         for (Key key2 : map.keySet()) {
             if (key2.equals(key)) return true;
         }
         return false;
     }
 
-    public static <Key, Value> void removeFromIdMap(Map<Key, Value> map, Key key) {
+    public static <Key, Value> void removeFromMap(Map<Key, Value> map, Key key) {
         for (Key key2 : map.keySet()) {
             if (key2.equals(key)) {
                 map.remove(key2);
             }
         }
+    }
+
+    /**
+     * @deprecated Use {@link MapUtil#hasKey(Map, Object)} instead.
+     */
+    @Deprecated
+    public static <Key, Value> boolean hasKeyIdMap(Map<Key, Value> map, Key key) {
+        return MapUtil.hasKey(map, key);
+    }
+
+    /**
+     * @deprecated Use {@link MapUtil#removeFromMap(Map, Object)} instead.
+     */
+    @Deprecated
+    public static <Key, Value> void removeFromIdMap(Map<Key, Value> map, Key key) {
+        MapUtil.removeFromMap(map, key);
     }
 }
